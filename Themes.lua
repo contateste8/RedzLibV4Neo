5c7ec8ed83389df4380dd5c0e1148881d74752f5
Ok0iSUV52ZSsLnD <contateste1910101@gmail.com>
2025-11-05 22:09:35 -0300

    Update Themes.lua

--- Themes.lua
+++ Themes.lua
@@ -15,6 +15,23 @@ local Themes = {
     ["Stroke Thickness"] = 1,
     ["Tween Style"] = Enum.EasingStyle.Quad
   },
+
+  ["ProtectionMenu"] = {
+    ["Color Hub 1"] = Color3.fromRGB(18, 18, 18),
+    ["Color Hub 2"] = Color3.fromRGB(25, 25, 25),
+    ["Color Theme"] = Color3.fromRGB(189, 246, 254),
+    ["Color Text"] = Color3.fromRGB(240, 240, 240),
+    ["Color Dark Text"] = Color3.fromRGB(200, 200, 200),
+    ["Color Stroke"] = Color3.fromRGB(80, 80, 80),
+    ["Corner Radius"] = UDim.new(0, 5),
+    ["Font"] = {
+      Enum.Font.GothamBlack,
+      Enum.Font.GothamBold,
+      Enum.Font.GothamMedium
+    },
+    ["Stroke Thickness"] = 1,
+    ["Tween Style"] = Enum.EasingStyle.Quad
+  },
 }
 
 return Themes
